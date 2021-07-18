7e1fe416bd857c46370d8a1c3e51a9e6b7ecadb0
Hamza <Hamza5955>
2021-07-18 23:27:34 +0200

    fine

--- Mail/main.js
+++ Mail/main.js
@@ -1,3 +1,13 @@
 /*Chiedi all’utente la sua email,
 controlla che sia nella lista di chi può accedere,
 stampa un messaggio appropriato sull’esito del controllo.*/
+
+var mail_list = ["loki@marvel.com", "info@hamzajam.it", "a@a.com"];
+
+var email = prompt("Inserisci la tua email", "a@a.com");
+
+if (mail_list.includes(email)) {
+  alert("benvenuto");
+} else {
+  alert("la tua mail non e presente tra le registrate");
+}
