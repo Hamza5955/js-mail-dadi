6188bc351e6012a21fc41a18eed6c53197acd56d
Hamza <Hamza5955>
2021-07-18 23:38:55 +0200

    comenti

--- Mail/main.js
+++ Mail/main.js
@@ -2,10 +2,11 @@
 controlla che sia nella lista di chi può accedere,
 stampa un messaggio appropriato sull’esito del controllo.*/
 
+//creo un arrey con le mail
 var mail_list = ["loki@marvel.com", "info@hamzajam.it", "a@a.com"];
-
+//chiedo al utente la mail
 var email = prompt("Inserisci la tua email", "a@a.com");
-
+//verifico che la mail indserita sia inclusa nel arrey se si stampo benvenuto seno stampo mail non presente
 if (mail_list.includes(email)) {
   alert("benvenuto");
 } else {
