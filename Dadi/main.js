944eea97553daf04584cc818f7c347ed22f020fe
Hamza <Hamza5955>
2021-07-19 00:41:57 +0200

    commenti

--- Dadi/main.js
+++ Dadi/main.js
@@ -2,14 +2,17 @@
 Stabilire il vincitore, in base a chi fa il punteggio più alto.*/
 var giocatore = Math.floor(Math.random() * 6) + 1;
 console.log(giocatore);
-
+//genero numero rand da 1-6 per giocatore
 var computer = Math.floor(Math.random() * 6) + 1;
 console.log(computer);
-
+//genero numero rand da 1-6 per computer
 if (giocatore > computer) {
+  //verifico che il numero generato da giocatore sia maggiore al quelo dell computer se si stampo il giocatore vince
   document.getElementById("dadi").innerHTML = "Il giocatore vince";
 } else if (giocatore < computer) {
+  //verifico che il numero generato da giocatore dia minore al quelo dell computer se si stampo il pc vince
   document.getElementById("dadi").innerHTML = "Il computer vince ";
 } else if (giocatore == computer) {
+  //verifico che il numero generato da giocatore sia uguale a quello generto dal pc poi stampo pareggio
   document.getElementById("dadi").innerHTML = "pareggiano";
 }
